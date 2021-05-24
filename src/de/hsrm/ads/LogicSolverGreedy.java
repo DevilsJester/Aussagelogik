3e2aa84cb6beb4322006e334cf477f10a7f0436a
Finalboss Tom <tomskosko@gmail.com>
2021-05-24 21:05:38 +0200

    Testcomment added again

--- src/de/hsrm/ads/LogicSolverGreedy.java
+++ src/de/hsrm/ads/LogicSolverGreedy.java
@@ -2,7 +2,7 @@ package de.hsrm.ads;
 
 public class LogicSolverGreedy {
 
-    //Testcomment
+    //testcomment
 
     static boolean satisfies(short[] assignment, short[] clause) {
         for (int i=0; i<clause.length; ++i)
