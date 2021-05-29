a45e7914ff9d5718add3254cd221efa66e46229c
Finalboss Tom <tomskosko@gmail.com>
2021-05-29 11:56:53 +0200

    changed to better tests

--- src/de/hsrm/ads/LogicSolverBacktrackingTest.java
+++ src/de/hsrm/ads/LogicSolverBacktrackingTest.java
@@ -1,6 +1,7 @@
 
 package de.hsrm.ads;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import org.junit.Test;
@@ -40,40 +41,58 @@ public class LogicSolverBacktrackingTest {
 		{ 0, 0, 0, 1 },
 	};
 
+	public static void check(String msg, short[] a, short[] b) {
+		assertEquals(msg, a.length, b.length);
+		for (int i=0; i<a.length; ++i) {
+			assertEquals(msg, a[i], b[i]);
+		}
+	}
+
 
 	// ( -X1 v -X2 ) ^ ( X2 v -X3 ) ^ ( X3 v X4 )
 	@Test
 	public void testBacktracking1() {
 		short[] result = LogicSolverBacktracking.solveBacktracking(formula1);
-		assertTrue( Arrays.equals(new short[] {-1,-1,-1,1}, result) );
+		//assertTrue( Arrays.equals(new short[] {-1,-1,-1,1}, result) );
+		check( "Backtracking Solution for ( -X1 v -X2 ) ^ ( X2 v -X3 ) ^ ( X3 v X4 ) should be (-1,-1,-1,1)!",
+				new short[] {-1,-1,-1,1}, result);
+
 	}
 
 	// ( -X1 v -X2 ) ^ ( -X1 v X2 ) ^ ( X1 v -X2 )
 	@Test
 	public void testBacktracking2() {
 		short[] result = LogicSolverBacktracking.solveBacktracking(formula2);
-		assertTrue( Arrays.equals(new short[] {-1,-1}, result) );
+		//assertTrue( Arrays.equals(new short[] {-1,-1}, result) );
+		check( "Backtracking Solution for ( -X1 v -X2 ) ^ ( -X1 v X2 ) ^ ( X1 v -X2 ) should be (-1,-1)!",
+				new short[] {-1,-1}, result);
 	}
 	
 	// ( -X1 v -X2 ) ^ ( -X1 v X2 ) ^ ( X1 v -X2 ) ^ ( X1 v X2 )
 	@Test
 	public void testBacktracking3() {
 		short[] result = LogicSolverBacktracking.solveBacktracking(formula3);
-		assertTrue( Arrays.equals(new short[] {0,0}, result) );
+		//assertTrue( Arrays.equals(new short[] {0,0}, result) );
+		check( "Backtracking Solution for ( -X1 v -X2 ) ^ ( -X1 v X2 ) ^ ( X1 v -X2 ) ^ ( X1 v X2 ) should be (0,0)!",
+				new short[] {0,0}, result);
 	}
 
 	// ( -X1 v -X2 ) ^ X1
 	@Test
 	public void testBacktracking4() {
 		short[] result = LogicSolverBacktracking.solveBacktracking(formula4);
-		assertTrue( Arrays.equals(new short[] {1,-1}, result) );
+		//assertTrue( Arrays.equals(new short[] {1,-1}, result) );
+		check( "Backtracking Solution for ( -X1 v -X2 ) ^ X1 should be (1,-1)!",
+				new short[] {1,-1}, result);
 	}
 
 	// X4 (nur X4)
 	@Test
 	public void testBacktracking5() {
 		short[] result = LogicSolverBacktracking.solveBacktracking(formula5);
-		assertTrue( Arrays.equals(new short[] {-1,-1,-1,1}, result) );
+		//assertTrue( Arrays.equals(new short[] {-1,-1,-1,1}, result) );
+		check( "Backtracking Solution for X4 should be (-1,-1,-1,1)!",
+				new short[] {-1,-1,-1,1}, result);
 	}
 	
 }
